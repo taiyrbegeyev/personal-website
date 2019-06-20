4c23487b3a63ffc5b0572cea9ceb565b6d5a11c3
pro100taiyr <taiyrbegeyev@gmail.com>
2019-06-20 20:56:38 +0200

    :sparkles: add intro section

---
+++ src/containers/Intro/Intro.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import {BackgroundImage, RVH, NameContainer, Title, SubTitle, ScrollDown} from './styles';
+import desktopImage from 'assets/IMG_1758.jpg';
+import mobileImage from 'assets/IMG_1766.jpg';
+
+const Intro = (props) => {
+  const [windowWidth, setWindowWidth ] = useState(window.innerWidth);
+  const imageUrl = windowWidth >= 650 ? desktopImage : mobileImage;
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    }
+  });
+
+  return(
+    <BackgroundImage imageUrl={imageUrl} >
+      <RVH>
+        <NameContainer>
+          <Title>
+            <h1>Taiyr Begeyev</h1>
+          </Title>
+          <SubTitle>
+            <p>Software Engineer</p>
+          </SubTitle>
+        </NameContainer>
+        <ScrollDown onClick={() => props.fullpageApi.moveSectionDown()} />
+      </RVH>
+    </BackgroundImage>
+  );
+};
+
+export default Intro;
