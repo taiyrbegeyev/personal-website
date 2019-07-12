02c79a05cc1356568da271ba6c2c6e0b7d7bd044
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-07-12 22:48:23 +0200

    :construction: add new components for AboutMe page

--- src/containers/AboutMe/AboutMe.jsx
+++ src/containers/AboutMe/AboutMe.jsx
@@ -1,15 +1,41 @@
 import React, { useState } from 'react';
 import SocialMedia from 'components/SocialMedia/SocialMedia';
 import NavBar from 'components/NavBar/NavBar';
+import { 
+  MainContainer, AboutMeWrapper,
+  AboutMeImage, AboutMePicture,
+  AboutMeInfo, AboutMeInfoSection,
+  AboutMePictureWrapper
+} from './styles';
+import profilePic from 'assets/taiyr.png';
+import profileImg from 'assets/taiyrKid.jpg';
 
 const AboutMe = (props) => {
   const [windowWidth ] = useState(window.innerWidth);
 
   return(
-    <React.Fragment>
+    <MainContainer>
+      <AboutMeWrapper>
+        <AboutMePictureWrapper>
+          <AboutMePicture src={profilePic}/>
+        </AboutMePictureWrapper>
+
+        <AboutMeInfoSection>
+          <AboutMeImage imageUrl={profileImg} />
+          <AboutMeInfo>
+            <h2>Taiyr Begeyev</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quam qui aliquam nulla et accusamus, accusantium ipsam iusto quae ullam repudiandae quos ex enim molestiae aut repellendus exercitationem soluta praesentium.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quam qui aliquam nulla et accusamus, accusantium ipsam iusto quae ullam repudiandae quos ex enim molestiae aut repellendus exercitationem soluta praesentium.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quam qui aliquam nulla et accusamus, accusantium ipsam iusto quae ullam repudiandae quos ex enim molestiae aut repellendus exercitationem soluta praesentium.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quam qui aliquam nulla et accusamus, accusantium ipsam iusto quae ullam repudiandae quos ex enim molestiae aut repellendus exercitationem soluta praesentium.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quam qui aliquam nulla et accusamus, accusantium ipsam iusto quae ullam repudiandae quos ex enim molestiae aut repellendus exercitationem soluta praesentium.</p>
+
+          </AboutMeInfo>
+        </AboutMeInfoSection>  
+      </AboutMeWrapper>
       <NavBar/>
       <SocialMedia />
-    </React.Fragment>
+    </MainContainer>
   );
 }
 
--- src/containers/AboutMe/styles.jsx
+++ src/containers/AboutMe/styles.jsx
@@ -1,1 +1,48 @@
 import styled from 'styled-components';
+import {Paper} from '@material-ui/core';
+import { device } from 'config/device';
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const AboutMeWrapper = styled(Paper)`
+  width: 80vw;
+  background-color: gray;
+  display: flex;
+  
+  @media ${device.tablet} {
+    width: 40vw;
+  }
+`
+
+export const AboutMeInfoSection = styled.div`
+  width: 100%;
+`
+
+export const AboutMeImage = styled.div`
+  background-image: url("${props => props.imageUrl}");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 15rem;
+`
+
+export const AboutMeInfo = styled.div`
+  height: auto;
+  font-size: 1.6rem;
+`
+
+export const AboutMePictureWrapper = styled.div`
+  position: absolute;
+  margin-top: 8.6rem;
+  margin-left: 1rem;
+`
+
+export const AboutMePicture = styled.img`
+  border-radius: 50%;
+  border: 0.4rem solid white;
+  width: 12.8rem;
+`
